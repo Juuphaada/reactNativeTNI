8e1dc8d0fe886752f1e57afb3b78761bbe51894e
Juuphaada <Ink545228705@gmail.com>
2024-07-13 11:10:50 +0700

    MyFirstProject

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import AppFooter from './components/AppFooter';
+import AppHeader from './components/AppHeader';
 
-export default function App() {
+export default function App(): React.JSX.Element{
+  const onClickMe = () => {
+    Alert.alert('Hey','hello React.js');
+  };
+
+  const users =[
+    {id:1001,name:'John'},
+    {id:1001,name:'Mary'}
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppHeader title="This is header" year={2002}/>
+      <AppHeader title="This is header2"/> {/*ใช่ซ้ำได้ */}
+
+      <Text>Hello React Native!</Text>
+      {
+        users.map((data,index)=>{
+        return <Text key={data.id}> No. {index} id: {data.id} name: {data.name}</Text>
+      })
+      }
+
+      <Button title="Click Me" onPress={()=>{
+        Alert.alert("Hi","React Native is Fun!!")}} color="blue"/>
+      <AppFooter/>
     </View>
   );
 }
